cc6c95fcb13c415f7fe8bad4adcbaf2ee5e1dede
hydrationn <suhwaparkk1031@gmail.com>
2024-03-21 06:54:57 +0900

    Array 과제 완료

---
+++ src/lab20240319/ArrayAssignment.java
@@ -0,0 +1,55 @@
+package lab20240319;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class ArrayAssignment {
+    public static void main(String[] args) {
+        try {
+            File file = new File("/Users/hydrationn/datastructure01/input.txt");
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+
+            // 한 줄씩 읽어서 가능하도록
+            String line = br.readLine(); // 라인 별로 읽어주고
+
+            while (line != null) {
+                StringTokenizer parser = new StringTokenizer(line, " "); // 한 라인 안에서 " "을 기준으로 읽어준다.
+                int length = parser.countTokens(); // 공백을 기준으로 parser.countTockens으로 배열 길이 정하기
+
+                // 길이 알았으니 배열 세워주고, 채워주기
+                int[] arr = new int[length];
+                for(int i = 0; i < length; i++) {
+                    arr[i] = Integer.parseInt(parser.nextToken());
+                }
+
+                System.out.print("INPUT: ");
+                for(int i = 0; i < length; i++) {
+                    System.out.print(arr[i] + " ");
+                }
+
+                System.out.println();
+
+                System.out.print("OUTPUT: ");
+                for(int i = length-1; i >= 0; i--) {
+                    if(arr[i] % 2 == 0) {
+                        System.out.print(arr[i] + " ");
+                    }
+                }
+                for(int i = length-1; i >= 0; i--) {
+                    if(arr[i] % 2 != 0) {
+                        System.out.print(arr[i] + " ");
+                    }
+                }
+                System.out.println();
+
+                line = br.readLine();
+            }
+            br.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("FileNotFoundException");
+        } catch (IOException e) {
+            throw new RuntimeException("IOException");
+        }
+    }
+}
