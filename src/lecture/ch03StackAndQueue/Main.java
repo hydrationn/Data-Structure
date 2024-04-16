a98726f65dfbdca9209a913bc755ce253e46d9a2
hydrationn <suhwaparkk1031@gmail.com>
2024-04-16 13:16:12 +0900

    3.2 Queue

--- src/lecture/ch03StackAndQueue/Main.java
+++ src/lecture/ch03StackAndQueue/Main.java
@@ -15,5 +15,19 @@ public class Main {
         listStack.push("peach");
         System.out.println(listStack.peek());
         listStack.pop();
+
+        ArrayQueue<String> arrayQueue = new ArrayQueue<String>();
+        arrayQueue.add("lemon");
+        arrayQueue.add("mango");
+        arrayQueue.add("lime");
+        arrayQueue.remove();
+        arrayQueue.remove();
+
+        ListQueue<String> listQueue = new ListQueue<String>();
+        listQueue.add("kiwi");
+        listQueue.add("citron");
+        listQueue.add("litch");
+        listQueue.remove();
+        listQueue.remove();
     }
 }
